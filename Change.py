6e19cab4f26fea8f8480e574fc32336227791b26
OrbitAIM <marketingai@epack.in>
2024-07-23 12:07:35 +0530

    Update Change.py

--- Change.py
+++ Change.py
@@ -26,7 +26,11 @@ docs = pdf_loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 splits = text_splitter.split_documents(docs)
 
-embeddings= HuggingFaceEmbeddings(
+
+   model_name = "sentence-transformers/all-mpnet-base-v2"
+model_kwargs = {'device': 'cpu'}
+encode_kwargs = {'normalize_embeddings': False}
+embeddings = HuggingFaceEmbeddings(
     model_name=model_name,
     model_kwargs=model_kwargs,
     encode_kwargs=encode_kwargs
